e7f697b0e6684474db5d9aaaea56cce952a27762
box-key <kei.box.fight@gmail.com>
2020-05-17 18:13:16 -0400

    broadcast lookup table

--- test_pyspark.py
+++ test_pyspark.py
@@ -356,3 +356,33 @@ class TestPySpark:
                 if year[0] != 'OLS_COEF':
                     count += year[1]
         assert count == 49
+
+    def test_broadcast(self):
+        """ Broadcast lookup table """
+        # load lookup table
+        with open('data\\nyc_cscl.csv', 'r') as f:
+            file = csv.DictReader(f)
+            lookup = [row for row in file]
+        # broadcast lookup table
+        lookup_bcast = sc.broadcast(lookup)
+        file = 'violation_small.csv'
+        # skip headers
+        data = sc.textFile(file)
+        header = data.first()
+        # load data
+        res = sc.textFile(file) \
+                .filter(lambda x: x != header) \
+                .mapPartitions(lambda x: csv.reader(x)) \
+                .map(lambda x: (int(dt.datetime.strptime(x[4], '%m/%d/%Y').year), x[21], x[23], x[24])) \
+                .filter(lambda x: (2015 <= x[0] and x[0] <= 2019)) \
+                .map(lambda x: (x[0], countyname2borocode(x[1]), x[2], x[3])) \
+                .filter(lambda x: x[1] > 0) \
+                .map(lambda x: (x[0], street_segmentid_lookup(x[2], x[3], x[1], lookup_bcast.value))) \
+                .filter(lambda x: int(x[1]) > 0) \
+                .map(lambda x: ((x[1], x[0]), 1)) \
+                .reduceByKey(lambda x, y: x + y) \
+                .sortByKey(True, 1) \
+                .map(lambda x: (x[0][0], [(x[0][1], x[1])])) \
+                .reduceByKey(lambda x, y: x + y) \
+                .mapValues(lambda x: self.fill_zer0(x) + [('OLS_COEF', self.ols(x))]) \
+                .collect()
